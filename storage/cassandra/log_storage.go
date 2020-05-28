57ded1eadf9d7dba992ec98e3be5f1b3f24570df
Paul Hadfield <paul@phad.org.uk>
2020-05-28 22:13:27 +0100

    [WIP] QueueLeaves copied from mysql storage layer.

--- storage/cassandra/log_storage.go
+++ storage/cassandra/log_storage.go
@@ -1,13 +1,16 @@
 package cassandra
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"sort"
 	"time"
 
 	"github.com/golang/glog"
+	"github.com/golang/protobuf/ptypes"
 	"github.com/google/trillian"
 	"github.com/google/trillian/merkle/hashers"
 	"github.com/google/trillian/monitoring"
@@ -15,6 +18,8 @@ import (
 	"github.com/google/trillian/storage/cache"
 	"github.com/google/trillian/types"
 	"github.com/monzo/gocassa"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var (
@@ -49,7 +54,7 @@ func (m *cassLogStorage) CheckDatabaseAccessible(context.Context) error {
 }
 
 type cassLogTX struct {
-  ks gocassa.KeySpace
+	ks gocassa.KeySpace
 }
 
 // readOnlyLogTX implements storage.ReadOnlyLogTX
@@ -123,16 +128,44 @@ func (m *cassLogStorage) SnapshotForTree(ctx context.Context, tree *trillian.Tre
 	return tx, err
 }
 
-func (m *cassLogStorage) QueueLeaves(_ context.Context, tree *trillian.Tree, leaves []*trillian.LogLeaf, _ time.Time) ([]*trillian.QueuedLogLeaf, error) {
+func (m *cassLogStorage) QueueLeaves(ctx context.Context, tree *trillian.Tree, leaves []*trillian.LogLeaf, queueTimestamp time.Time) ([]*trillian.QueuedLogLeaf, error) {
 	glog.Infof("cassLogStorage.QueueLeaves: %d leaves for tree %v", len(leaves), tree)
-	return nil, errors.New("cassLogStorage.QueueLeaves: not implemented")
+	tx, err := m.beginInternal(ctx, tree)
+	if tx != nil {
+		// Ensure we don't leak the transaction if an error occurs below.
+		defer tx.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+	existing, err := tx.QueueLeaves(ctx, leaves, queueTimestamp)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return nil, err
+	}
+
+	ret := make([]*trillian.QueuedLogLeaf, len(leaves))
+	for i, e := range existing {
+		if e != nil {
+			ret[i] = &trillian.QueuedLogLeaf{
+				Leaf:   e,
+				Status: status.Newf(codes.AlreadyExists, "leaf already exists: %v", e.LeafIdentityHash).Proto(),
+			}
+			continue
+		}
+		ret[i] = &trillian.QueuedLogLeaf{Leaf: leaves[i]}
+	}
+	return ret, nil
 }
 
 type logTreeTX struct {
 	treeTX
-	ks  gocassa.KeySpace
+	ks   gocassa.KeySpace
 	root types.LogRootV1
-	slr *trillian.SignedLogRoot
+	slr  *trillian.SignedLogRoot
 }
 
 func (t *logTreeTX) ReadRevision(ctx context.Context) (int64, error) {
@@ -147,8 +180,127 @@ func (t *logTreeTX) DequeueLeaves(ctx context.Context, limit int, cutoffTime tim
 	return nil, errors.New("cassLogStorage.logTreeTX.DequeueLeaves: not implemented")
 }
 
+// sortLeavesForInsert returns a slice containing the passed in leaves sorted
+// by LeafIdentityHash, and paired with their original positions.
+// QueueLeaves and AddSequencedLeaves use this to make the order that LeafData
+// row locks are acquired deterministic and reduce the chance of deadlocks.
+func sortLeavesForInsert(leaves []*trillian.LogLeaf) []leafAndPosition {
+	ordLeaves := make([]leafAndPosition, len(leaves))
+	for i, leaf := range leaves {
+		ordLeaves[i] = leafAndPosition{leaf: leaf, idx: i}
+	}
+	sort.Sort(byLeafIdentityHashWithPosition(ordLeaves))
+	return ordLeaves
+}
+
 func (t *logTreeTX) QueueLeaves(ctx context.Context, leaves []*trillian.LogLeaf, queueTimestamp time.Time) ([]*trillian.LogLeaf, error) {
-	return nil, errors.New("cassLogStorage.logTreeTX.QueueLeaves: not implemented")
+	glog.Infof("cassLogStorage.logTreeTX.QueueLeaves: will queue %d leaves.", len(leaves))
+
+	t.treeTX.mu.Lock()
+	defer t.treeTX.mu.Unlock()
+
+	// Don't accept batches if any of the leaves are invalid.
+	for _, leaf := range leaves {
+		if len(leaf.LeafIdentityHash) != t.hashSizeBytes {
+			return nil, fmt.Errorf("queued leaf must have a leaf ID hash of length %d", t.hashSizeBytes)
+		}
+		var err error
+		leaf.QueueTimestamp, err = ptypes.TimestampProto(queueTimestamp)
+		if err != nil {
+			return nil, fmt.Errorf("got invalid queue timestamp: %v", err)
+		}
+	}
+	start := time.Now()
+
+	ordLeaves := sortLeavesForInsert(leaves)
+	existingCount := 0
+	existingLeaves := make([]*trillian.LogLeaf, len(leaves))
+
+	for _, ol := range ordLeaves {
+		i, leaf := ol.idx, ol.leaf
+
+		leafStart := time.Now()
+		qTimestamp, err := ptypes.Timestamp(leaf.QueueTimestamp)
+		if err != nil {
+			return nil, fmt.Errorf("got invalid queue timestamp: %v", err)
+		}
+		_ = qTimestamp
+		//		_, err = t.tx.ExecContext(ctx, insertLeafDataSQL, t.treeID, leaf.LeafIdentityHash, leaf.LeafValue, leaf.ExtraData, qTimestamp.UnixNano())
+		insertDuration := time.Since(leafStart)
+		_ = insertDuration
+		if isDuplicateErr(err) {
+			// Remember the duplicate leaf, using the requested leaf for now.
+			existingLeaves[i] = leaf
+			existingCount++
+			continue
+		}
+		if err != nil {
+			glog.Warningf("Error inserting %d into LeafData: %s", i, err)
+			return nil, err
+		}
+
+		// Create the work queue entry
+		args := []interface{}{
+			t.treeID,
+			leaf.LeafIdentityHash,
+			leaf.MerkleLeafHash,
+		}
+		queueTimestamp, err := ptypes.Timestamp(leaf.QueueTimestamp)
+		if err != nil {
+			return nil, fmt.Errorf("got invalid queue timestamp: %v", err)
+		}
+		args = append(args, queueArgs(t.treeID, leaf.LeafIdentityHash, queueTimestamp)...)
+		// _, err = t.tx.ExecContext(
+		// 	ctx,
+		// 	insertUnsequencedEntrySQL,
+		// 	args...,
+		// )
+		if err != nil {
+			glog.Warningf("Error inserting into Unsequenced: %s", err)
+			return nil, err
+		}
+		leafDuration := time.Since(leafStart)
+		_ = leafDuration
+	}
+	insertDuration := time.Since(start)
+	_ = insertDuration
+
+	if existingCount == 0 {
+		return existingLeaves, nil
+	}
+
+	// For existing leaves, we need to retrieve the contents.  First collate the desired LeafIdentityHash values.
+	var toRetrieve [][]byte
+	for _, existing := range existingLeaves {
+		if existing != nil {
+			toRetrieve = append(toRetrieve, existing.LeafIdentityHash)
+		}
+	}
+	results, err := t.getLeafDataByIdentityHash(ctx, toRetrieve)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve existing leaves: %v", err)
+	}
+	if len(results) != len(toRetrieve) {
+		return nil, fmt.Errorf("failed to retrieve all existing leaves: got %d, want %d", len(results), len(toRetrieve))
+	}
+	// Replace the requested leaves with the actual leaves.
+	for i, requested := range existingLeaves {
+		if requested == nil {
+			continue
+		}
+		found := false
+		for _, result := range results {
+			if bytes.Equal(result.LeafIdentityHash, requested.LeafIdentityHash) {
+				existingLeaves[i] = result
+				found = true
+				break
+			}
+		}
+		if !found {
+			return nil, fmt.Errorf("failed to find existing leaf for hash %x", requested.LeafIdentityHash)
+		}
+	}
+	return existingLeaves, nil
 }
 
 func (t *logTreeTX) AddSequencedLeaves(ctx context.Context, leaves []*trillian.LogLeaf, timestamp time.Time) ([]*trillian.QueuedLogLeaf, error) {
@@ -171,6 +323,14 @@ func (t *logTreeTX) GetLeavesByHash(ctx context.Context, leafHashes [][]byte, or
 	return nil, errors.New("cassLogStorage.logTreeTX.GetLeavesByHash: not implemented")
 }
 
+// getLeafDataByIdentityHash retrieves leaf data by LeafIdentityHash, returned
+// as a slice of LogLeaf objects for convenience.  However, note that the
+// returned LogLeaf objects will not have a valid MerkleLeafHash, LeafIndex, or IntegrateTimestamp.
+func (t *logTreeTX) getLeafDataByIdentityHash(ctx context.Context, leafHashes [][]byte) ([]*trillian.LogLeaf, error) {
+	return nil, errors.New("cassLogStorage.logTreeTX.getLeafDataByIdentityHash: not implemented")
+
+}
+
 func (t *logTreeTX) LatestSignedLogRoot(ctx context.Context) (*trillian.SignedLogRoot, error) {
 	glog.Infof("cassandra.logTreeTX.LatestSignedLogRoot(): slr=%v", t.slr)
 	return t.slr, nil
@@ -180,11 +340,11 @@ func (t *logTreeTX) LatestSignedLogRoot(ctx context.Context) (*trillian.SignedLo
 func (t *logTreeTX) fetchLatestRoot(ctx context.Context) (*trillian.SignedLogRoot, error) {
 	treesTable := t.ks.Table("trees", &cassTree{}, gocassa.Keys{
 		PartitionKeys: []string{"tree_id"},
-  }).WithOptions(gocassa.Options{TableName: "trees"})
+	}).WithOptions(gocassa.Options{TableName: "trees"})
 
 	tree := cassTree{}
 	if err := treesTable.Where(gocassa.Eq("tree_id", t.treeID)).ReadOne(&tree).Run(); err != nil {
-	  return nil, err
+		return nil, err
 	}
 	if len(tree.CurrentSignedLogRootJSON) == 0 {
 		return nil, storage.ErrTreeNeedsInit
@@ -216,7 +376,7 @@ func (t *logTreeTX) StoreSignedLogRoot(ctx context.Context, root *trillian.Signe
 
 	treesTable := t.ks.Table("trees", &cassTree{}, gocassa.Keys{
 		PartitionKeys: []string{"tree_id"},
-  }).WithOptions(gocassa.Options{TableName: "trees"})
+	}).WithOptions(gocassa.Options{TableName: "trees"})
 
 	treeHeadsTable := t.ks.Table("tree_heads", &cassTreeHead{}, gocassa.Keys{
 		PartitionKeys: []string{"tree_id", "revision", "timestamp_nanos"},
@@ -226,7 +386,7 @@ func (t *logTreeTX) StoreSignedLogRoot(ctx context.Context, root *trillian.Signe
 		"current_slr_json": data,
 		"root_signature":   root.LogRootSignature,
 	}).Add(treeHeadsTable.Set(cassTreeHead{
-		TreeID:         t.treeID, // PK
+		TreeID:         t.treeID,         // PK
 		Revision:       logRoot.Revision, // PK
 		TimestampNanos: logRoot.TimestampNanos,
 		Size:           logRoot.TreeSize,
@@ -241,4 +401,38 @@ func (t *logTreeTX) StoreSignedLogRoot(ctx context.Context, root *trillian.Signe
 
 func (t *logTreeTX) UpdateSequencedLeaves(ctx context.Context, leaves []*trillian.LogLeaf) error {
 	return errors.New("cassLogStorage.logTreeTX.UpdateSequencedLeaves: not implemented")
+}
+
+// leafAndPosition records original position before sort.
+type leafAndPosition struct {
+	leaf *trillian.LogLeaf
+	idx  int
+}
+
+// byLeafIdentityHashWithPosition allows sorting (as above), but where we need
+// to remember the original position
+type byLeafIdentityHashWithPosition []leafAndPosition
+
+func (l byLeafIdentityHashWithPosition) Len() int {
+	return len(l)
+}
+func (l byLeafIdentityHashWithPosition) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+func (l byLeafIdentityHashWithPosition) Less(i, j int) bool {
+	return bytes.Compare(l[i].leaf.LeafIdentityHash, l[j].leaf.LeafIdentityHash) == -1
+}
+
+func isDuplicateErr(err error) bool {
+	// switch err := err.(type) {
+	// case *mysql.MySQLError:
+	// 	return err.Number == errNumDuplicate
+	// default:
+	// 	return false
+	// }
+	return false
+}
+
+func queueArgs(_ int64, _ []byte, queueTimestamp time.Time) []interface{} {
+	return []interface{}{queueTimestamp.UnixNano()}
 }
--- storage/cassandra/tree_storage.go
+++ storage/cassandra/tree_storage.go
@@ -1,51 +1,57 @@
 package cassandra
 
 import (
-  "context"
-  "errors"
+	"context"
+	"errors"
+	"sync"
 
-  "github.com/golang/glog"
+	"github.com/golang/glog"
 	"github.com/google/trillian"
 	"github.com/google/trillian/storage/cache"
-  "github.com/google/trillian/storage/tree"
+	"github.com/google/trillian/storage/tree"
 )
 
 type cassTreeStorage struct {
 }
 
-func (c *cassTreeStorage) beginTreeTx(_ context.Context, tree *trillian.Tree, _/*hashSizeBytes*/ int, _ *cache.SubtreeCache) (treeTX, error) {
-  return treeTX{
-    treeID: tree.TreeId,
-  }, nil
+func (c *cassTreeStorage) beginTreeTx(_ context.Context, tree *trillian.Tree, _ /*hashSizeBytes*/ int, _ *cache.SubtreeCache) (treeTX, error) {
+	return treeTX{
+		mu:     &sync.Mutex{},
+		treeID: tree.TreeId,
+	}, nil
 }
 
 type treeTX struct {
-  treeID int64
-  writeRevision int64
+	// mu ensures that ks can only be used for one query at a time.
+	mu            *sync.Mutex
+	treeID        int64
+	writeRevision int64
+	hashSizeBytes int
 }
 
 func (t *treeTX) Commit(ctx context.Context) error {
-  glog.Infof("cassandra.treeTx.Commit: no-op")
-  return nil
+	glog.Infof("cassandra.treeTx.Commit: no-op")
+	return nil
 }
 
 func (t *treeTX) Rollback() error {
-  return errors.New("cassandra.treeTx.Rollback not implemented")
+	return errors.New("cassandra.treeTx.Rollback not implemented")
 }
 
 func (t *treeTX) Close() error {
-  return errors.New("cassandra.treeTx.Close not implemented")
+	glog.Infof("cassandra.treeTx.Close: no-op")
+	return nil
 }
 
 func (t *treeTX) GetMerkleNodes(ctx context.Context, treeRevision int64, nodeIDs []tree.NodeID) ([]tree.Node, error) {
-  return nil, errors.New("cassandra.treeTx.GetMerkleNodes not implemented")
+	return nil, errors.New("cassandra.treeTx.GetMerkleNodes not implemented")
 }
 
 func (t *treeTX) SetMerkleNodes(ctx context.Context, nodes []tree.Node) error {
-  return errors.New("cassandra.treeTx.SetMerkleNodes not implemented")
+	return errors.New("cassandra.treeTx.SetMerkleNodes not implemented")
 }
 
 func (t *treeTX) IsOpen() bool {
-  panic("cassandra.treeTx.Commit not implemented")
-  return false
+	panic("cassandra.treeTx.Commit not implemented")
+	return false
 }
